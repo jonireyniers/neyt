61c9cb0590e00dc87e495de2677604681601ab69
joni <jonireyniers@hotmail.be>
2026-03-04 20:38:53 +0100

    feat: add sitemap integration and enhance SEO metadata across pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.neytfurniture.com',
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr', 'nl'],
